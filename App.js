b60c77dd2d09a9b34a047d3536399f0847fc9415
mutzon <martinutzon@gmail.com>
2024-12-24 13:13:26 +0100

    permissions65 færdig

--- App.js
+++ App.js
@@ -3,8 +3,35 @@ import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { Home } from "./pages/Home/Home";
 import { ImageBackground } from "react-native";
 import backGroundImg from "./assets/background.png";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [coordinates, setCoordinates] = useState();
+
+  useEffect(() => {
+    getUserCoordinates();
+  }, []);
+
+  useEffect(() => {}, []);
+
+  async function getUserCoordinates() {
+    const { status } = await requestForegroundPermissionsAsync();
+    if (status === "granted") {
+      const location = await getCurrentPositionAsync();
+      setCoordinates({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      });
+    } else {
+      setCoordinates({ lat: "48.85", lng: "2.35" });
+    }
+  }
+  console.log(coordinates);
+
   return (
     <ImageBackground
       imageStyle={s.img}
