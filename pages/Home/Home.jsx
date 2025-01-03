33183014a3f57212bd6d2196a26496149cb4498f
mutzon <martinutzon@gmail.com>
2025-01-03 16:02:19 +0100

    tester bare

--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -8,7 +8,7 @@ export function Home() {
         <Text style={s.txt}>Basic Weather info</Text>
       </View>
       <View style={s.searchbar_container}>
-        <Text style={s.txt}>SearchBar</Text>
+        <Text style={s.txt}>SearchBar test</Text>
       </View>
       <View style={s.meteo_advanced}>
         <Text style={s.txt}>Advanced Weather info</Text>
